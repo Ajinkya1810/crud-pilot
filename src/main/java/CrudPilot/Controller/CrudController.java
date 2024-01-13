1311437b70ec6290e506a6d191a68682a3e074fb
Ajinkya <ajinkyatikhe18@gmail.com>
2024-01-13 17:45:50 +0530

    logger added and response entity

--- src/main/java/CrudPilot/Controller/CrudController.java
+++ src/main/java/CrudPilot/Controller/CrudController.java
@@ -1,25 +1,34 @@
 package CrudPilot.Controller;
 
 import CrudPilot.Executor.CrudExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class CrudController {
 
+    Logger logger = LoggerFactory.getLogger(CrudController.class);
+
     @Autowired
     CrudExecutor crudExecutor;
 
     @PostMapping("/createTable")
-    public String save(@RequestBody Map<String, Object> request){
-        System.out.println(request);
+    public ResponseEntity<Map<String, Object>> save(@RequestBody Map<String, Object> request) {
+        logger.info("Create Table called. Request received : {}", request);
+        if (crudExecutor.createTable(request)) {
+            return new ResponseEntity<>(request, HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>(request, HttpStatus.BAD_REQUEST);
+        }
 
-        crudExecutor.createTable(request);
-
-        return "save() called";
     }
 }
--- src/main/java/CrudPilot/Executor/CrudExecutor.java
+++ src/main/java/CrudPilot/Executor/CrudExecutor.java
@@ -1,6 +1,9 @@
 package CrudPilot.Executor;
 
 import CrudPilot.Constants.DataModelConstants;
+import CrudPilot.Controller.CrudController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -10,23 +13,26 @@ import java.util.*;
 @Service
 public class CrudExecutor {
 
+    Logger logger = LoggerFactory.getLogger(CrudController.class);
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public String createTable(Map<String, Object> request){
-
-        String createTableSqlQuery = "CREATE TABLE " + request.get(DataModelConstants.ELEMENT_NAME) +
-                " (id INT AUTO_INCREMENT PRIMARY KEY, ";
-
-        createTableSqlQuery = createTableSqlQuery + createColumnFromConfiguration(request.get(DataModelConstants.CONFIGURATION)) + ")";
-
-        jdbcTemplate.execute(createTableSqlQuery);
-
-        return "CrudExecutor save() called";
+    public boolean createTable(Map<String, Object> request) {
+        try {
+            String createTableSqlQuery = "CREATE TABLE " + request.get(DataModelConstants.ELEMENT_NAME) +
+                    " (id INT AUTO_INCREMENT PRIMARY KEY, ";
+            createTableSqlQuery = createTableSqlQuery + createColumnFromConfiguration(request.get(DataModelConstants.CONFIGURATION)) + ")";
+            jdbcTemplate.execute(createTableSqlQuery);
+            logger.info("Query generated for the createTable : {}", createTableSqlQuery);
+            return true;
+        } catch (Exception e) {
+            logger.error("Exception occurred in createTable probable cause : ", e);
+            return false;
+        }
     }
 
     public String createColumnFromConfiguration(Object configuration) {
-
         String createColumnQuery = "";
         LinkedHashMap<Object, Object> linkedHashMap = (LinkedHashMap<Object, Object>) configuration;
         Set<Object> keys = linkedHashMap.keySet();
@@ -37,4 +43,19 @@ public class CrudExecutor {
         return createColumnQuery.substring(0, createColumnQuery.length() - 2);
     }
 
+    public String  insertDataUsingConfiguration(Object configuration) {
+        LinkedHashMap<Object, Object> linkedHashMap = (LinkedHashMap<Object, Object>) configuration;
+        Set<Object> keys = linkedHashMap.keySet();
+        String columns = "";
+        String values = "";
+        for (Object key : keys) {
+            columns = columns + key.toString() + ", ";
+            values = values + linkedHashMap.get(key).toString() + ", ";
+        }
+        columns = columns.substring(0, columns.length() - 2);
+        columns = columns + " ) VALUES ( ";
+        values = values.substring(0, values.length() - 2);
+        values = values + " )";
+        return "";
+    }
 }
